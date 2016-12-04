ae2fb87170275836b9502eaa0cd6006d81447733
Dominik <dominik.malantowicz@gmail.com>
2016-12-04 21:05:10 +0100

    Have login header

--- app/src/main/java/pl/krk/droidcon/workshops/login/LoginActivity.kt
+++ app/src/main/java/pl/krk/droidcon/workshops/login/LoginActivity.kt
@@ -1,5 +1,12 @@
 package pl.krk.droidcon.workshops.login
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import pl.krk.droidcon.workshops.R
 
-class LoginActivity : AppCompatActivity()
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}

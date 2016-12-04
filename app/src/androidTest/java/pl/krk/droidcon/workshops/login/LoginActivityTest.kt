2f07399a79fd16cab4d52dcdddc01ac4f52289b2
Dominik <dominik.malantowicz@gmail.com>
2016-12-04 20:55:31 +0100

    Login Activity init test

---
+++ app/src/androidTest/java/pl/krk/droidcon/workshops/login/LoginActivityTest.kt
@@ -0,0 +1,17 @@
+package pl.krk.droidcon.workshops.login
+
+import android.support.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class LoginActivityTest {
+
+    @Rule
+    @JvmField
+    val rule = ActivityTestRule(LoginActivity::class.java)
+
+    @Test
+    fun initTest() {
+
+    }
+}

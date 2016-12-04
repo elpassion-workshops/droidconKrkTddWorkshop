ca7799f698094d5684a209b1ded43465dcef0f17
Dominik <dominik.malantowicz@gmail.com>
2016-12-04 20:19:25 +0100

    Show error when api call fails

--- app/src/test/java/pl/krk/droidcon/workshops/login/LoginControllerTest.kt
+++ app/src/test/java/pl/krk/droidcon/workshops/login/LoginControllerTest.kt
@@ -1,17 +1,21 @@
 package pl.krk.droidcon.workshops.login
 
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.never
-import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.*
+import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito.verify
+import rx.Observable
 
 class LoginControllerTest {
 
     private val api = mock<Login.Api>()
     private val view = mock<Login.View>()
     private val controller = LoginController(api, view)
+
+    @Before
+    fun setUp() {
+        whenever(api.login(any(), any())).thenReturn(Observable.never())
+    }
 
     @Test
     fun shouldCallApiOnLoginWithCorrectLoginAndPassword() {
@@ -49,6 +53,13 @@ class LoginControllerTest {
         verify(api, never()).login(any(), any())
     }
 
+    @Test
+    fun shouldShowErrorWhenApiCallFails() {
+        whenever(api.login("login", "password")).thenReturn(Observable.error(RuntimeException()))
+        controller.onLogin("login", "password")
+        verify(view, times(1)).showLoginFailedError()
+    }
+
 }
 
 class LoginController(val api: Login.Api, val view: Login.View) {
@@ -56,17 +67,18 @@ class LoginController(val api: Login.Api, val view: Login.View) {
         if (login.isEmpty() || password.isEmpty()) {
             view.showEmptyCredentialError()
         } else {
-            api.login(login, password)
+            api.login(login, password).subscribe({}, { view.showLoginFailedError() })
         }
     }
 }
 
 interface Login {
     interface Api {
-        fun login(login: String, password: String)
+        fun login(login: String, password: String): Observable<Unit>
     }
 
     interface View {
         fun showEmptyCredentialError()
+        fun showLoginFailedError()
     }
 }

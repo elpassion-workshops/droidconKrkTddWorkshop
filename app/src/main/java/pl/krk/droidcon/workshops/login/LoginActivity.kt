1a66c9f859487ac4fed1681c291ef9e8bd62f7e5
Kasper Kondzielski <kasper.kondzielski@elpassion.pl>
2016-12-06 18:01:47 +0100

    Open next screen when call to api succeed

--- app/src/main/java/pl/krk/droidcon/workshops/login/LoginActivity.kt
+++ app/src/main/java/pl/krk/droidcon/workshops/login/LoginActivity.kt
@@ -1,9 +1,11 @@
 package pl.krk.droidcon.workshops.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.login_activity.*
 import pl.krk.droidcon.workshops.R
+import pl.krk.droidcon.workshops.next.NextScreenActivity
 
 class LoginActivity : AppCompatActivity(), Login.View {
     private val controller by lazy {
@@ -28,7 +30,7 @@ class LoginActivity : AppCompatActivity(), Login.View {
     }
 
     override fun openNextScreen() {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+        startActivity(Intent(this, NextScreenActivity::class.java))
     }
 
     override fun showLoginFailedError() {

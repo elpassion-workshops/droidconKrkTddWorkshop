0024868d538f77e3f18588364ce117e11605a4be
ghostbuster91 <kghost0@gmail.com>
2016-12-09 19:22:36 +0100

    Pass facebook button provider created by facebook login creator

--- app/src/test/java/pl/krk/droidcon/workshops/login/LoginControllerTest.kt
+++ app/src/test/java/pl/krk/droidcon/workshops/login/LoginControllerTest.kt
@@ -1,22 +1,17 @@
 package pl.krk.droidcon.workshops.login
 
 import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Test
 
 class LoginControllerTest {
 
     val view = mock<Login.View>()
     val api = mock<Login.Api>()
-    val fbButtonProvider = mock<Login.FacebookButtonProvider>()
-    val loginController = LoginController(view, fbButtonProvider, api)
-
-    @Test
-    fun shouldPassFacebookButtonProviderToViewOnCreate() {
-        loginController.onCreate()
-
-        verify(view).addFacebookButton(fbButtonProvider)
-    }
+    val facebookLoginCreator = mock<Login.FacebookLoginCreator>()
+    val loginController = LoginController(view, facebookLoginCreator, api)
 
     @Test
     fun shouldCallApiWithFbTokenAfterLoginSucceed() {
@@ -29,14 +24,22 @@ class LoginControllerTest {
         loginController.onFacebookLoginError()
         verify(view).showLoginFailsError()
     }
+
+    @Test
+    fun shouldPassFacebookButtonProviderCreatedByFacebookLoginCreator() {
+        val facebookButtonProvider = mock<Login.FacebookButtonProvider>()
+        whenever(facebookLoginCreator.create()).thenReturn(facebookButtonProvider)
+        loginController.onCreate()
+        verify(view, times(1)).addFacebookButton(facebookButtonProvider)
+    }
 }
 
 class LoginController(private val view: Login.View,
-                      private val fbButtonProvider: Login.FacebookButtonProvider,
+                      private val facebookLoginCreator: Login.FacebookLoginCreator,
                       private val api: Login.Api) {
 
     fun onCreate() {
-        view.addFacebookButton(fbButtonProvider)
+        view.addFacebookButton(facebookLoginCreator.create())
     }
 
     fun onFacebookLoginSuccess(token: String) {
@@ -51,6 +54,10 @@ class LoginController(private val view: Login.View,
 interface Login {
 
     interface FacebookButtonProvider
+
+    interface FacebookLoginCreator {
+        fun  create(): FacebookButtonProvider
+    }
 
     interface View {
         fun addFacebookButton(fbButtonProvider: FacebookButtonProvider)

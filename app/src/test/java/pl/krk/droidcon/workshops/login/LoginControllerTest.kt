0c32b5aecfb65fa60dbff3bab87eac114430fac0
ghostbuster91 <kghost0@gmail.com>
2016-12-10 10:07:56 +0100

    Call api onLogin

--- app/src/test/java/pl/krk/droidcon/workshops/login/LoginControllerTest.kt
+++ app/src/test/java/pl/krk/droidcon/workshops/login/LoginControllerTest.kt
@@ -1,12 +1,27 @@
 package pl.krk.droidcon.workshops.login
 
-import org.junit.Assert.assertTrue
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
 import org.junit.Test
 
 class LoginControllerTest {
 
     @Test
-    fun shouldPass() {
-        assertTrue(true)
+    fun shouldCallApiOnLogin() {
+        val api = mock<Login.Api>()
+        LoginController(api).onLogin()
+        verify(api).login()
+    }
+}
+
+class LoginController(private val api: Login.Api) {
+    fun onLogin() {
+        api.login()
+    }
+}
+
+interface Login {
+    interface Api {
+        fun login()
     }
 }

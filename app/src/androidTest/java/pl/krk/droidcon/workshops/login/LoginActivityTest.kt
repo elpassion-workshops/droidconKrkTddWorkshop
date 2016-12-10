3f8398f4d41261b1539e3bbffc2d059554c6b791
Agnieszka <agnieszka.adamczyk@miquido.com>
2016-12-10 15:27:12 +0100

    Typed Password Be Starred

--- app/src/androidTest/java/pl/krk/droidcon/workshops/login/LoginActivityTest.kt
+++ app/src/androidTest/java/pl/krk/droidcon/workshops/login/LoginActivityTest.kt
@@ -1,6 +1,11 @@
 package pl.krk.droidcon.workshops.login
 
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.assertion.ViewAssertions.*
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.rule.ActivityTestRule
+import android.text.InputType
 import com.elpassion.android.commons.espresso.*
 import org.junit.Rule
 import org.junit.Test
@@ -36,5 +41,12 @@ class LoginActivityTest {
                 .isDisplayed()
                 .typeText("abcdef19")
                 .hasText("abcdef19")
+    }
+
+    @Test
+    fun shouldTypedPasswordBeStarred() {
+        onId(R.id.loginPasswordInput)
+                .typeText("abcdef19")
+                .check(matches(withInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)))
     }
 }

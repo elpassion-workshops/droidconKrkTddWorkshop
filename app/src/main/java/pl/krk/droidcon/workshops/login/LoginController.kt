cb64c152f607184c36fdecf2acc2da2836adc227
Ahmed Isam <suda.nix2010@gmail.com>
2016-12-10 14:09:23 +0300

    [Refactor] extract validation to method

--- app/src/main/java/pl/krk/droidcon/workshops/login/LoginController.kt
+++ app/src/main/java/pl/krk/droidcon/workshops/login/LoginController.kt
@@ -2,8 +2,11 @@ package pl.krk.droidcon.workshops.login
 
 class LoginController(private val api: Login.Api, private val view: Login.View) {
 
+    private fun credentialsAreValid(email: String, password: String) : Boolean =
+            (password.length >= 3) and email.isNotEmpty() and password.isNotEmpty()
+
     fun onLogin(email: String, password: String) {
-        if ((password.length >= 3) and email.isNotEmpty() and password.isNotEmpty()) {
+        if (credentialsAreValid(email, password)) {
             view.hideError()
             api.login(email, password).subscribe({ unit ->  } ,
                     { throwable -> view.showError("API login failed")})

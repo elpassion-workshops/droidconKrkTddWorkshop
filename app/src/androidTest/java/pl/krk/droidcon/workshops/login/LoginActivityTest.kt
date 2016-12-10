74e3baa33dc04ad1dbfdec3f6b369ca6e130a73f
Karol Kowalski <ordon1313@gmail.com>
2016-12-10 14:57:06 +0100

    Email hint be visible

--- app/src/androidTest/java/pl/krk/droidcon/workshops/login/LoginActivityTest.kt
+++ app/src/androidTest/java/pl/krk/droidcon/workshops/login/LoginActivityTest.kt
@@ -1,8 +1,14 @@
 package pl.krk.droidcon.workshops.login
 
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.rule.ActivityTestRule
+import com.elpassion.android.commons.espresso.isDisplayed
+import com.elpassion.android.commons.espresso.onText
 import org.junit.Rule
 import org.junit.Test
+import pl.krk.droidcon.workshops.R
 
 class LoginActivityTest {
 
@@ -11,6 +17,11 @@ class LoginActivityTest {
     val rule = ActivityTestRule(LoginActivity::class.java)
 
     @Test
-    fun shouldStartActivity() {
+    fun shouldEmailHintBeVisible() {
+        onText(R.string.loginEmailHeader).isDisplayed()
+
+        // version without android.commons below
+        Espresso.onView(ViewMatchers.withText(R.string.loginEmailHeader))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }

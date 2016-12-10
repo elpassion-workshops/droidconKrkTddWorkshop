19920a8df27826b4df8880c4b26caf46f74ecec2
Karol Kowalski <ordon1313@gmail.com>
2016-12-10 15:47:07 +0100

    Show error when login fails

--- app/src/androidTest/java/pl/krk/droidcon/workshops/login/LoginActivityTest.kt
+++ app/src/androidTest/java/pl/krk/droidcon/workshops/login/LoginActivityTest.kt
@@ -1,29 +1,35 @@
 package pl.krk.droidcon.workshops.login
 
-import android.support.test.espresso.assertion.ViewAssertions
-import android.support.test.espresso.assertion.ViewAssertions.*
-import android.support.test.espresso.matcher.ViewMatchers
-import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withInputType
 import android.support.test.rule.ActivityTestRule
 import android.text.InputType
 import com.elpassion.android.commons.espresso.*
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Rule
 import org.junit.Test
 import pl.krk.droidcon.workshops.R
+import rx.Observable
 
 class LoginActivityTest {
 
+    val api = mock<Login.Api>()
+
     @Rule
     @JvmField
-    val rule = ActivityTestRule(LoginActivity::class.java)
+    val rule = ActivityTestRule(LoginActivity::class.java, false, false)
 
     @Test
     fun shouldEmailHintBeVisible() {
+        startActivity()
         onText(R.string.loginEmailHeader).isDisplayed()
     }
-    
+
     @Test
     fun shouldHaveTypedEmailInTheInput() {
+        startActivity()
         onId(R.id.loginEmailInput)
                 .isDisplayed()
                 .typeText("email@test.pl")
@@ -32,11 +38,13 @@ class LoginActivityTest {
 
     @Test
     fun shouldPasswordHintBeVisible() {
+        startActivity()
         onText(R.string.loginPasswordHeader).isDisplayed()
     }
 
     @Test
     fun shouldHaveTypedPasswordInTheInput() {
+        startActivity()
         onId(R.id.loginPasswordInput)
                 .isDisplayed()
                 .typeText("abcdef19")
@@ -45,6 +53,7 @@ class LoginActivityTest {
 
     @Test
     fun shouldTypedPasswordBeStarred() {
+        startActivity()
         onId(R.id.loginPasswordInput)
                 .typeText("abcdef19")
                 .check(matches(withInputType(InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD)))
@@ -52,6 +61,19 @@ class LoginActivityTest {
 
     @Test
     fun shouldLoginButtonBeEnabled() {
+        startActivity()
         onId(R.id.loginButton).isEnabled()
+    }
+
+    @Test
+    fun shouldShowErrorWhenLoginFails() {
+        whenever(api.login(any(), any())).thenReturn(Observable.error(RuntimeException()))
+        startActivity()
+        onId(R.id.loginButton).click()
+        onId(R.id.loginError).isDisplayed()
+    }
+
+    private fun startActivity() {
+        rule.launchActivity(null)
     }
 }
